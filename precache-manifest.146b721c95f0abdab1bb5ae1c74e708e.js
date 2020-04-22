cb80564f396963439b9b0ed0c2897cae3959d47e
kvo189 <kvo189@gmail.com>
2020-04-22 15:41:47 -0500

    Updates

---
+++ precache-manifest.146b721c95f0abdab1bb5ae1c74e708e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4aabed34c6ce8afc41a7aa3ab312510",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "bb6e0507caad57614bc8",
+    "url": "/tic-tac-toe/static/css/main.bbc6b3ac.chunk.css"
+  },
+  {
+    "revision": "38c58a404a3fb46dd5f4",
+    "url": "/tic-tac-toe/static/js/2.b102e267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.b102e267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb6e0507caad57614bc8",
+    "url": "/tic-tac-toe/static/js/main.f70a9a19.chunk.js"
+  },
+  {
+    "revision": "2e3e4e44d6a5c7256d12",
+    "url": "/tic-tac-toe/static/js/runtime-main.f4158612.js"
+  }
+]);
